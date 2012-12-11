5f58ce0311427f76e5a757566e8094a58082996e
juanan <juanan.pereira@ehu.es>
2012-12-12 00:38:30 +0100

    mst implementations. first attempt

--- src/mst.cpp
+++ src/mst.cpp
@@ -1,34 +1,33 @@
 /*
  * Pseudocode
-Prim_Sparse( Vertex s )
-Begin
-	// G = (V, E)
-	for each v IN V
-		dist[v] = infinite
-		parent[v] = 0 // treat 0 as a special null vertex
-		hPos[v] = 0 // indicates that v NOT IN heap
-	h = new Heap // priority queue (heap) initially empty
-	h.insert(s) // s will be the root of the MST
+ Prim_Sparse( Vertex s )
+ Begin
+ // G = (V, E)
+ for each v IN V
+ dist[v] = infinite
+ parent[v] = 0 // treat 0 as a special null vertex
+ hPos[v] = 0 // indicates that v NOT IN heap
+ h = new Heap // priority queue (heap) initially empty
+ h.insert(s) // s will be the root of the MST
 
-	while (not h.isEmpty() ) 	// should repeat |V|-1 times
-		v = h.remove()       	// add v to the MST
-		dist[v] = -dist[v]   	// marks v as now in the MST
-		for each u IN  adj(v)   // examine each neighbour u of v
-			if wgt(v, u) < dist[u]
-				dist[u] = wgt(v, u)
-				parent[u] = v
-				if u NOT IN  h
-					h.insert( u)
-				else
-					h.siftUp( hpos[u])
-			end if
-		end for
-	end while
+ while (not h.isEmpty() ) 	// should repeat |V|-1 times
+ v = h.remove()       	// add v to the MST
+ dist[v] = -dist[v]   	// marks v as now in the MST
+ for each u IN  adj(v)   // examine each neighbour u of v
+ if wgt(v, u) < dist[u]
+ dist[u] = wgt(v, u)
+ parent[u] = v
+ if u NOT IN  h
+ h.insert( u)
+ else
+ h.siftUp( hpos[u])
+ end if
+ end for
+ end while
 
-	return parent
-End
+ return parent
+ End
  */
-
 
 #include <algorithm>
 #include <fstream>
@@ -45,39 +44,37 @@ struct arco {
 };
 
 struct graphNode {
- int index;
- vector<arco> vecinos;
+	int index;
+	int valor;
+	vector<arco> vecinos;
 };
 
-void readFile(string name, vector<graphNode>& lista){
+void readFile(string name, vector<graphNode>& lista) {
 
 	ifstream in(name.c_str());
 	int numVertex, numEdges, src, dst, wgt;
 
 	if (not in)
 		perror(name.c_str());
-	else
-	{
+	else {
 		in >> numVertex;
 		in >> numEdges;
 
 		cout << numVertex << "************" << endl;
-		lista.resize(numVertex+1);
+		lista.resize(numVertex + 1);
 
 		graphNode dummy;
-		dummy.index=0;
-		lista[0]=dummy;
+		dummy.index = 0;
+		lista[0] = dummy;
 
-
-
-		for(int i = 1; i <= numVertex; i++){
+		for (int i = 1; i <= numVertex; i++) {
 			graphNode nodo;
 			nodo.index = i;
 			vector<arco> vecinos;
 
 			nodo.vecinos = vecinos;
 
-			lista.at(i) =  nodo ;
+			lista.at(i) = nodo;
 		}
 
 		while (in >> src >> dst >> wgt) {
@@ -98,56 +95,110 @@ void readFile(string name, vector<graphNode>& lista){
 	}
 }
 
-int main () {
-	vector<graphNode> lista;
+#include <limits>
+#include <algorithm>
 
-	string file("/home/juanan/Dropbox/Public/datastructures2/edgesj.txt");
-	readFile(file, lista);
+typedef std::greater<int> less;
 
-//	std::ostream_iterator< int > output( cout, " " );
+bool comp(const graphNode& a, const graphNode& b){
 
-	cout << "------------------------" << endl;
+	return a.valor < b.valor;
+}
 
-	for ( vector<graphNode>::iterator it(lista.begin()+1); it != lista.end(); ++it ){
-		cout << it->index << " ";
 
-		vector<arco> vecinos( it->vecinos);
-		for ( vector<arco>::iterator vec (vecinos.begin()); vec != vecinos.end(); ++vec){
-			 cout << vec->index << " ";
-		}
+void mst(vector<graphNode> ex, const int numVertex, vector<graphNode> lista)
+{
+	vector<int> dist(numVertex, numeric_limits<int>::max());
+	vector<int> padre(numVertex);
 
-		cout << endl;
+	graphNode pop() {
+		pop_heap(ex.begin(), ex.end(), comp);
+		return ex.pop_back();
 	}
 
-	// std::copy( lista.begin(), lista.end(), output );
+	void push(graphNode v) {
+		ex.push_back(v);
+		push_heap(ex.begin(), ex.end(), comp);
+	}
+
+
+	ex.push_back(lista.at(1));
+	make_heap(ex.begin(), ex.end(), comp);
+
+	while (ex.size() > 0)
+	{
+		int v = pop();
+		dist.at(v) = -dist.at(v);
+		vector<arco> vecinos(lista.at(v).vecinos);
+
+		for (vector<arco>::iterator u(vecinos.begin()); u != vecinos.end(); ++u)
+		{
+			if (u->weight < dist.at(u->index))
+			{
+				dist.at(u->index) = u->weight;
+				padre.at(u->index) = v;
+				if (find(ex.begin(), ex.end(), u->index) != ex.end())
+					dist.at(u->index) = u->weight;
+				else
+					push(lista.at(u->index));
+
+			}
+		}
+	}
 
 }
 
 
-int main2 () {
-  int myints[] = {10,20,30,5,15};
-  vector<int> v(myints,myints+5);
-  std::ostream_iterator< int > output( cout, " " );
+void main() {
+vector<graphNode> lista;
 
-  typedef std::greater<int> less;
+string file("/home/juanan/Dropbox/Public/datastructures2/edgesj.txt");
+readFile(file, lista);
 
-  make_heap (v.begin(),v.end(), less());
-  cout << "initial max heap   : " << v.front() << endl;
+//	std::ostream_iterator< int > output( cout, " " );
 
-  pop_heap (v.begin(),v.end(), less()); v.pop_back();
-  cout << "max heap after pop : " << v.front() << endl;
+cout << "------------------------" << endl;
 
-  v.push_back(99); push_heap (v.begin(),v.end(), less());
-  cout << "max heap after push: " << v.front() << endl;
+for (vector<graphNode>::iterator it(lista.begin() + 1); it != lista.end();
+		++it) {
+	cout << it->index << " ";
 
-  sort_heap (v.begin(),v.end(),less());
+	vector<arco> vecinos(it->vecinos);
+	for (vector<arco>::iterator vec(vecinos.begin()); vec != vecinos.end();
+			++vec) {
+		cout << vec->index << " ";
+	}
 
-  cout << "final sorted range :";
-  std::copy( v.begin(), v.end(), output );
+	cout << endl;
+}
 
+// std::copy( lista.begin(), lista.end(), output );
 
-  cout << endl;
+}
 
-  return 0;
+int main2() {
+int myints[] = { 10, 20, 30, 5, 15 };
+vector<int> v(myints, myints + 5);
+std::ostream_iterator<int> output(cout, " ");
+
+make_heap(v.begin(), v.end(), less());
+cout << "initial max heap   : " << v.front() << endl;
+
+pop_heap(v.begin(), v.end(), less());
+v.pop_back();
+cout << "max heap after pop : " << v.front() << endl;
+
+v.push_back(99);
+push_heap(v.begin(), v.end(), less());
+cout << "max heap after push: " << v.front() << endl;
+
+sort_heap(v.begin(), v.end(), less());
+
+cout << "final sorted range :";
+std::copy(v.begin(), v.end(), output);
+
+cout << endl;
+
+return 0;
 }
 
